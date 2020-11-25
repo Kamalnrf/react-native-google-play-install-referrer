27f8947a83c9a8dc419c9a61f4ed46c3ff426493
Kamal Mukkamala <kamalnrf@gmail.com>
2020-11-25 14:19:10 +0530

    refactor(ios module): remove ios module as it is not required

--- example/ios/File.swift
+++
@@ -1,6 +0,0 @@
-//
-//  File.swift
-//  PlayInstallReferrerExample
-//
-
-import Foundation
--- ios/PlayInstallReferrer.h
+++
@@ -1,5 +0,0 @@
-#import <React/RCTBridgeModule.h>
-
-@interface PlayInstallReferrer : NSObject <RCTBridgeModule>
-
-@end
